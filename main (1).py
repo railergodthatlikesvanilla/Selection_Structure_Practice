29fd935d945057959961d43989ce6cb6ca6b1cb6
railergodthatlikesvanilla <thirdybigbird@gmail.com>
2026-09-07 15:25:55 +0800

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,7 @@
+number = float(input("Enter a number: "))
+#This makes the number variable
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+#This uses the variable and tells whether it's an odd or even number
